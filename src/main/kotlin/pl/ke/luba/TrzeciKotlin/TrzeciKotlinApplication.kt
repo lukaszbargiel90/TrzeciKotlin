b586f1d2e7dfe9ca6aaa87edcfb8732399d3f163
lukaszbargiel90 <lukasz-50@wp.pl>
2020-11-04 12:55:24 +0100

    first commit

---
+++ src/main/kotlin/pl/ke/luba/TrzeciKotlin/TrzeciKotlinApplication.kt
@@ -0,0 +1,11 @@
+package pl.ke.luba.TrzeciKotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TrzeciKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<TrzeciKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/pl/ke/luba/TrzeciKotlin/TrzeciKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.ke.luba.TrzeciKotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TrzeciKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
